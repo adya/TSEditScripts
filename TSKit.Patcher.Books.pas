ba59b70073235ce70af4f0a7d9143f7712414e9f
Arkadii Hlushchevskyi <adya.glu@gmail.com>
2019-08-22 22:28:19 +0300

    Fixed incorrect path in Books patcher.

--- TSKit.Patcher.Books.pas
+++ TSKit.Patcher.Books.pas
@@ -4,10 +4,10 @@ uses 'TSKit.Patcher';
 //==============================================================================
 function Initialize: integer;
 begin
-  AddRecord('Book Covers Skyrim.esp', 'BOOK \ OBND - Object Bounds');
+  AddRecord('Book Covers Skyrim.esp', 'OBND - Object Bounds');
   AddRecord('Book Covers Skyrim.esp', 'MODEL');
   AddRecord('Book Covers Skyrim.esp', 'INAM');
-  AddRecord('Book Covers Skyrim - Lost Library.esp', 'BOOK \ OBND - Object Bounds');
+  AddRecord('Book Covers Skyrim - Lost Library.esp', 'OBND - Object Bounds');
   AddRecord('Book Covers Skyrim - Lost Library.esp', 'MODEL');
   AddRecord('Book Covers Skyrim - Lost Library.esp', 'INAM');
   AddRecord('Dynamic Patch - ReWeight.Books.esp', 'DATA\Weight');
