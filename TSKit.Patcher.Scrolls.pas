573e5d13e1d19027f6993a814586b7819fc6fc35
Arkadii Hlushchevskyi <adya.glu@gmail.com>
2019-08-23 23:50:35 +0300

    Added patcher for scrolls.

---
+++ TSKit.Patcher.Scrolls.pas
@@ -0,0 +1,12 @@
+﻿unit TSKitPatcherLight;
+uses 'TSKit.Patcher';
+
+//==============================================================================
+function Initialize: integer;
+begin
+  AddRecord('Unofficial Skyrim Special Edition Patch.esp', 'KWDA - Keywords');
+  Patch('SCRL', 'Scrolls');
+  Result := 0;
+end;
+
+end.
