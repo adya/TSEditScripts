7af5c2250fb04ac39e7bd7e7905401d3d3a91fe0
Arkadii Hlushchevskyi <adya.glu@gmail.com>
2019-08-23 23:50:22 +0300

    Added patcher for factions

---
+++ TSKit.Patcher.Factions.pas
@@ -0,0 +1,15 @@
+﻿unit TSKitPatcherLight;
+uses 'TSKit.Patcher';
+
+//==============================================================================
+function Initialize: integer;
+begin
+  AddRecord('Wild World.esp', 'Relations');
+  AddRecord('Wild World.esp', 'DATA - Flags');
+  AddRecord('Crime Overhaul Expanded.esp', 'DATA - Flags');
+  AddRecord('OBIS SE.esp', 'Relations');
+  Patch('FACT', 'Factions');
+  Result := 0;
+end;
+
+end.
