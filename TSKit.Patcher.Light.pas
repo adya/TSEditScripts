a978df7e60c0add523e168d0e38d6d0b662494c2
Arkadii Hlushchevskyi <adya.glu@gmail.com>
2019-08-26 21:53:56 +0300

    Updated lights patcher with conflicting records.

--- TSKit.Patcher.Light.pas
+++ TSKit.Patcher.Light.pas
@@ -7,11 +7,13 @@ begin
   AddRecord('Luminosity Lighting Overhaul.esp', 'XCLL');
   AddRecord('OCW_Obscure''s_CollegeofWinterhold.esp', 'XCLL\Inherits');
   AddRecord('OCW_Obscure''s_CollegeofWinterhold.esp', 'LTMP');
-  AddRecord('Particle Patch for ENB SSE.esp', 'DATA');
+  AddRecord('Distinct Interiors.esp', 'DATA - Flags');
+  AddRecord('Particle Patch for ENB SSE.esp', 'DATA - Flags');
   AddRecord('Particle Patch for ENB SSE.esp', 'XCCM');
   AddRecord('Particle Patch for ENB SSE.esp', 'XCIM');
   AddRecord('Soulmancer Soundtrack - NoVindDun.esp', 'XCMO');
-  AddRecord('Blacksmith Forge Water Fix SE USSEP.esp', 'DATA');
+  AddRecord('Blacksmith Forge Water Fix SE USSEP.esp', 'DATA - Flags');
+  AddRecord('Unique Display Room.esp', 'XEZN - Encounter Zone');
   Patch('CELL', 'Light');
   Result := 0;
 end;
