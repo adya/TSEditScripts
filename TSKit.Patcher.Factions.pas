ba92cce992ad69df61af6fe0f7c44fb9b0c4bdf1
Arkadii Hlushchevskyi <adya.glu@gmail.com>
2019-08-26 21:53:41 +0300

    Updated factions patcher with conflicting records.

--- TSKit.Patcher.Factions.pas
+++ TSKit.Patcher.Factions.pas
@@ -4,9 +4,10 @@ uses 'TSKit.Patcher';
 //==============================================================================
 function Initialize: integer;
 begin
+  AddRecord('Crime Overhaul Expanded.esp', 'DATA - Flags');
+  AddRecord('Windhelm Bridge Overhaul.esp', 'DATA - Flags');
   AddRecord('Wild World.esp', 'Relations');
   AddRecord('Wild World.esp', 'DATA - Flags');
-  AddRecord('Crime Overhaul Expanded.esp', 'DATA - Flags');
   AddRecord('OBIS SE.esp', 'Relations');
   Patch('FACT', 'Factions');
   Result := 0;
